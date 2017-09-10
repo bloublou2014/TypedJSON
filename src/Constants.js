991822e0a6a7f90247e8fe132ed6bad13f4f328c
Your Name <you@example.com>
2017-09-10 06:42:45 -0400

    tests

--- src/Constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Constants = (function () {
-    function Constants() {
-    }
-    Constants.METADATA_FIELD_KEY = "__typedJsonJsonObjectMetadataInformation__";
-    return Constants;
-}());
-exports.Constants = Constants;
--- src/JsonMemberMetadata.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var JsonMemberMetadata = (function () {
-    function JsonMemberMetadata() {
-    }
-    return JsonMemberMetadata;
-}());
-exports.JsonMemberMetadata = JsonMemberMetadata;
